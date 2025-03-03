19bfe8b1381d706f0681bedfa7782030b5877272
DasVargas <daslynvargasgambo@gmail.com>
2025-03-02 22:26:49 -0600

    Prueba 1

--- src/main/java/cr/ac/una/unaplanilla/App.java
+++ src/main/java/cr/ac/una/unaplanilla/App.java
@@ -21,6 +21,7 @@ public class App extends Application {
         scene = new Scene(loadFXML("primary"), 640, 480);
         stage.setScene(scene);
         stage.show();
+      System.out.println("ola");
     }
     
     
