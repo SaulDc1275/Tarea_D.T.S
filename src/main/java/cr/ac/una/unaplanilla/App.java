75066cc48d71fedf24bf2ed69c8640d989a2bb01
SaulDc1275 <saul.chinchilla.badilla@est.una.ac.cr>
2025-03-04 17:16:26 -0600

    Prueba commit intellij

--- src/main/java/cr/ac/una/unaplanilla/App.java
+++ src/main/java/cr/ac/una/unaplanilla/App.java
@@ -15,9 +15,10 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        scene = new Scene(loadFXML("primary"), 1080, 720);
         stage.setScene(scene);
         stage.show();
+
     }
 
     static void setRoot(String fxml) throws IOException {
